ca975775e2a075cc448af079fc9d5c51a1f2c2cb
ehab <ehab41976@gmail.com>
2023-07-12 23:59:17 -0700

    current spots

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -4,11 +4,11 @@ const express = require('express')
 const bcrypt = require('bcryptjs');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { Spot, SpotImage, User } = require('../../db/models');
+const { Spot, SpotImage, Review, User } = require('../../db/models');
 
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
-// const reviewimage = require('../../db/models/reviewimage');
+
 
 
 
@@ -22,16 +22,89 @@ router.get('/', async (req, res) => {
 
     const spots = await Spot.findAll({
 
-        include: {
-            model: SpotImage,
-            // as:'previewImage',
+        include: [
+            { model: SpotImage },
+            { model: Review },
 
-        }
+        ]
 
     });
 
+
+
+    /////////////////////////////////////////////
     let spotsList = [];
+    // console.log(Review.stars)
     spots.forEach(spot => {
+        spotsList.push(spot.toJSON())
+
+    })
+
+    // console.log(spotsList)
+
+    spotsList.forEach(spot => {
+
+        spot.SpotImages.forEach(image => {
+            // console.log(image.preview)
+
+            if (image.preview === true) {
+                spot.previewImage = image.url
+            }
+        })
+        if (!spot.previewImage) {
+            spot.previewImage = " no image found"
+            // console.log(spot.previewImage)
+        }
+
+        delete spot.SpotImages
+    })
+    //////////////////////////////////////////
+    spotsList.forEach(spot => { // we accessed the spot1 review 1 then to next spot
+
+        const totalReviews = spot.Reviews.map(review => { // we accessed the review
+            return review.stars
+        })
+        // console.log(totalReviews)
+        let sumValues = 0;
+        let count = 0;
+        totalReviews.forEach(value => {
+            sumValues += value;
+            count++
+        });
+
+        let avgRating = count > 0 ? sumValues / count : 0;
+        spot.avgRating = avgRating
+        // console.log(avgRating)
+        delete spot.Reviews
+    })
+
+
+
+    ///////////////////////////////////
+
+    res.status(200)
+    res.json({ Spots: spotsList });
+
+});
+
+////////////////////////////////////////////
+
+router.get('/current', requireAuth, async (req, res) => {
+    const userId = req.user.id
+
+    const spotsCurrent = await Spot.findAll({
+        where: {
+            ownerId: userId
+        },
+        include: [
+            { model: SpotImage },
+            { model: Review }
+
+        ]
+    })
+
+    let spotsList = [];
+    spotsCurrent.forEach(spot => {
         spotsList.push(spot.toJSON())
 
     })
@@ -51,13 +124,41 @@ router.get('/', async (req, res) => {
 
         delete spot.SpotImages
     })
+//////////////////////////////////////////////////////
+spotsList.forEach(spot => { // we accessed the spot1 review 1 then to next spot
+
+    const totalReviews = spot.Reviews.map(review => { // we accessed the review
+        return review.stars
+    })
+    // console.log(totalReviews)
+    let sumValues = 0;
+    let count = 0;
+    totalReviews.forEach(value => {
+        sumValues += value;
+        count++
+    });
+
+    let avgRating = count > 0 ? sumValues / count : 0;
+    spot.avgRating = avgRating
+    // console.log(avgRating)
+    delete spot.Reviews
+})
+
+
+
+////////////////////////////////////////////////////
 
 
     res.status(200)
-    res.json({ Spots: spotsList });
+    res.json({ spots: spotsList })
+})
 
-});
 
-////////////////////////////////////////////
+/////////////////////////////////////////////
+
+
+
+
+
 
 module.exports = router;
