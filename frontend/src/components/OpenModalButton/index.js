e22d082367d59cef80ed77f8bf94c08643346c19
ehab <ehab41976@gmail.com>
2023-08-25 00:06:23 -0700

    login signup close menu fixed

--- frontend/src/components/OpenModalButton/index.js
+++ frontend/src/components/OpenModalButton/index.js
@@ -6,12 +6,17 @@ function OpenModalButton({
   buttonText, // text of the button that opens the modal
   onButtonClick, // optional: callback function that will be called once the button that opens the modal is clicked
   onModalClose, // optional: callback function that will be called once the modal is closed
+  onClose,
 }) {
   const { setModalContent, setOnModalClose } = useModal();
 
   const onClick = () => {
     if (typeof onButtonClick === "function") onButtonClick();
     if (typeof onModalClose === "function") setOnModalClose(onModalClose);
+    if (buttonText === "Log In" || buttonText === "Sign Up" ) {
+      onClose();
+    }
+
     setModalContent(modalComponent);
   };
 
